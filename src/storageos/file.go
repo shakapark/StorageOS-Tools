c37cb7547b1756f89deecfe20918a46cfef7a4ed
Shakapark <shakapark1994@gmail.com>
2020-01-21 09:09:26 +0100

    Use ioutil to read file id

--- src/storageos/file.go
+++ src/storageos/file.go
@@ -2,28 +2,19 @@ package storageos
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 )
 
 // GetFileID Get NodeID in local file
 func GetFileID(path string) (string, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return "", errors.New("Opening File: " + error.Error(err))
-	}
-	defer f.Close()
 
-	b1 := make([]byte, 64)
-	n1, err := f.Read(b1)
+	dat, err := ioutil.ReadFile(path)
 	if err != nil {
 		return "", errors.New("Reading File: " + error.Error(err))
 	}
 
-	if b1[n1-1] == 10 {
-		b1 = b1[:(n1 - 1)]
-	}
-
-	return string(b1), nil
+	return string(dat), nil
 }
 
 // ReplaceFileID Replace ID in file
